3f67897a3dc4dd5397d08571a955321c83e394bc
Adam Pecan <adamnyk@gmail.com>
2022-10-21 17:13:49 -0600

    flask sessions functionality added

--- 24.3 flask-survey/app.py
+++ 24.3 flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -9,8 +9,6 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 @app.route("/")
 def show_homepage():
@@ -18,17 +16,25 @@ def show_homepage():
     return render_template("home.html", survey=survey)
 
 
+@app.route("/reset_session", methods=["POST"])
+def reset_session():
+    """Post request to create and empty Flask session list of responses."""
+    session["responses"] = []
+
+    return redirect("questions/0")
+
+
 @app.route("/questions/<int:ques_id>")
 def show_question(ques_id):
     """Show question with given question id."""
 
-    if len(responses) == len(survey.questions):
-        flash("Submission already recieved.")
+    if len(session["responses"]) == len(survey.questions):
+        flash("Submission already recieved.", "success")
         return redirect("/thank_you")
 
-    if ques_id != len(responses):
-        flash("Redirecting you to the next question.")
-        return redirect(f"/questions/{len(responses)}")
+    if ques_id != len(session["responses"]):
+        flash("Redirecting you to the next question.", "error")
+        return redirect(f"/questions/{len(session['responses'])}")
 
     question = survey.questions[ques_id]
 
@@ -44,12 +50,15 @@ def post_answer():
     """Post request to submit answer to questions."""
 
     answer = request.form["answer"]
+
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
 
     if len(responses) == len(survey.questions):
         return redirect("/thank_you")
 
-    if len(responses) != len(survey.questions):
+    else:
         return redirect(f"/questions/{len(responses)}")
 
 
@@ -57,4 +66,4 @@ def post_answer():
 def show_thank_you():
     """Show thank-you page."""
 
-    return render_template("thank_you.html", responses=responses)
+    return render_template("thank_you.html")
