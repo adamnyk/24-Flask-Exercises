c8736353b3fb773d8a6e3ea630317119ed616a0c
Adam Pecan <adamnyk@gmail.com>
2022-10-20 23:17:57 -0600

    survey

--- 24.2 flask-madlibs/app.py
+++ 24.2 flask-madlibs/app.py
@@ -5,6 +5,7 @@ from stories import story, stories
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "app_key"
+
 debug = DebugToolbarExtension(app)
 
 
@@ -17,12 +18,12 @@ def home_page():
 @app.route("/form")
 def show_form():
     """Shows form for chosen Madlib story."""
-    
+
     story_id = request.args.get("story_id")
-            
+
     chosen_story = stories.get(int(story_id))
-            
-    return render_template("form.html", story = chosen_story, story_id=story_id)
+
+    return render_template("form.html", story=chosen_story, story_id=story_id)
 
 
 @app.route("/story")
@@ -30,7 +31,7 @@ def make_story():
     """Shows completed madlib story."""
     story_id = request.args.get("story_id")
     chosen_story = stories.get(int(story_id))
-    
+
     completed_story = chosen_story.generate(request.args)
 
-    return render_template("story.html", story=completed_story) 
+    return render_template("story.html", story=completed_story)
--- 24.3 flask-survey/app.py
+++ 24.3 flask-survey/app.py
@@ -1,7 +1,60 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "12345"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route("/")
+def show_homepage():
+    """Shows homepage"""
+    return render_template("home.html", survey=survey)
+
+
+@app.route("/questions/<int:ques_id>")
+def show_question(ques_id):
+    """Show question with given question id."""
+
+    if len(responses) == len(survey.questions):
+        flash("Submission already recieved.")
+        return redirect("/thank_you")
+
+    if ques_id != len(responses):
+        flash("Redirecting you to the next question.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[ques_id]
+
+    return render_template(
+        "questions.html",
+        question=question,
+        ques_id=ques_id,
+    )
+
+
+@app.route("/answer", methods=["POST"])
+def post_answer():
+    """Post request to submit answer to questions."""
+
+    answer = request.form["answer"]
+    responses.append(answer)
+
+    if len(responses) == len(survey.questions):
+        return redirect("/thank_you")
+
+    if len(responses) != len(survey.questions):
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/thank_you")
+def show_thank_you():
+    """Show thank-you page."""
+
+    return render_template("thank_you.html", responses=responses)
