3fa84b4d627307d5b6a0dc79828d74383b21d293
Adam Pecan <adamnyk@gmail.com>
2022-10-24 12:57:07 -0600

    unit testing

---
+++ 24.3 flask-survey/test_app.py
@@ -0,0 +1,32 @@
+from app import app, session
+from unittest import TestCase
+
+
+class SurveyTestCase(TestCase):
+    def test_start_page(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+
+            # Testing successful get request
+            self.assertEqual(res.status_code, 200)
+
+            # Testing root directory contains start button
+            self.assertIn("<button>Start the survey</button>", html)
+
+    def test_start_redirect(self):
+        with app.test.client() as client:
+            res = client.get('/reset_session')
+            
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, 'http://localhost/questions/0')
+            
+
+    def test_survey_answer(self):
+        # not working right now
+        with app.test_client() as client:
+            res = client.post("/answer", data={"answer", "Yes"})
+            responses = session["responses"]
+
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(responses[0], "Yes")
